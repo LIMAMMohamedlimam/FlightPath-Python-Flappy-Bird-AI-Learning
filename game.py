dc24c5974067ef63aad001b43a23668a9bed9ef8
LIMAMMohmedlimam <medlimamuia@gmail.com>
2024-02-24 20:47:43 +0100

    AI_model modifing the game and setting up the neat configurations

--- game.py
+++ game.py
@@ -1,10 +1,11 @@
-import pygame , time
+import pygame , time , neat
 from models import Disc , Bar
 from config import *
 
 class Game():
     def __init__(self):
-        self.disc = Disc()
+        
+        self.discs = []
         self.bars = []
         self.clock = pygame.time.Clock()
         self.score = 0
@@ -24,7 +25,18 @@ class Game():
         score_surface = SCORE_FONT.render(score_text , True , GREEN) 
         screen.blit(score_surface, [5,5])
 
-    def run(self , screen ):
+    def run(self , screen , genomes , config ):
+        gen = []
+        nets = []
+
+        for g in genomes :
+            net  = neat.nn.FeedForwardNetwork(g, config)
+            nets.append(net)
+            self.discs.append(Disc())
+            g.fitness = 0
+            gen.append(g)
+
+
         running = True
         self.start_time = time.time() #Initialisation du conteur 
         while running:
@@ -40,7 +52,8 @@ class Game():
             screen.fill(WHITE)
 
             if not self.game_over:
-                self.disc.update()
+                for disc in self.discs :
+                    disc.update()
 
                 if len(self.bars) == 0 or self.bars[-1].x < SCREEN_WIDTH - 300:
                     self.bars.append(Bar())
@@ -49,18 +62,25 @@ class Game():
                     bar.update()
 
                 self.bars = [bar for bar in self.bars if not bar.off_screen()]
+                #self.discs = [disc for ]
 
                 for bar in self.bars:
-                    if bar.collide(self.disc):
-                        self.game_over = True
-                    if not bar.passed and bar.x < self.disc.x:
-                        bar.passed = True
-                        self.score += 1
+                    for x ,disc in enumerate(self.discs) :
+                        if bar.collide(disc):
+                            #self.game_over = True
+                            gen[x].fitness -= 1 
+                            self.discs.pop(x)
+                            net.pop(x)
+                            gen.pop(x)
 
-                if self.disc.y <= 0 or self.disc.y >= SCREEN_HEIGHT:
-                    self.game_over = True
-
-            self.disc.draw(screen)
+                        if not bar.passed and bar.x < disc.x:
+                            bar.passed = True
+                            self.score += 1
+                for disc in self.discs :
+                    if disc.y <= 0 or disc.y >= SCREEN_HEIGHT:
+                        pass
+            for disc in self.discs:
+                disc.draw(screen)
             for bar in self.bars:
                 bar.draw(screen)
 
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 
-import time
+import time , neat
 import pygame
 import random
+import os
 from game import Game
 from config import SCREEN_HEIGHT , SCREEN_WIDTH
 
@@ -13,5 +14,21 @@ def main():
     pygame.quit()
 
 
+def run_ai(config_path):
+    config = neat.config.Config(neat.DefaultGenome , neat.DefaultReproduction ,
+                                neat.DefaultSpeciesSet , neat.DefaultStagnation,
+                                config_path)
+    
+    popul = neat.Population(config)
+    popul.add_reporter(neat.StdOutReporter(True))
+    status = neat.StatisticsReporter()
+    popul.add_reporter(status)
+
+    #winner = popul.run( ,50)
+
+
 if  __name__ == "__main__":
+    local_dir  = os.path.dirname(__file__)
+    config_path = os.path.joi,(local_dir,'NeatConfig.txt')
+    run_ai(config_path)
     main()
